f201eeb544229a245d2885cb32ce63eba4f2b1d3
radovanovic32416 <radovanovic32416@fon.bg.ac.rs>
2018-03-09 15:05:39 +0100

    Revert "Moj javni komentar"
    This reverts commit bf95c02f0d77e854f0d42be0a63e7db465cd8739.

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -29,7 +29,6 @@ public class Biblioteka implements BibliotekaInterfejs {
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(String naslov, Autor autor, String isbn, String izdavac) {
-		// Moj javni komentar
 		if (naslov == null)
 			throw new RuntimeException("Morate uneti naslov");
 
